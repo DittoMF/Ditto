3ce84ad81a43e561ea2a4973f78a79e174e5ea74
DittoMF <102764969+DittoMF@users.noreply.github.com>
2022-04-05 17:17:27 +0300

    Create HackerRank #3 A Very Big Sum.py
    Solution of HackerRank #3 A Very Big Sum Solution

---
+++ HackerRank #3 A Very Big Sum.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+  #Solution:
+  
+    sum = 0
+    for i in range(0,len(ar)):
+        sum = sum + ar[i]
+    return sum
+  
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
